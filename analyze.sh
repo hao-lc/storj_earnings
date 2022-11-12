83593a5df45037bea9dc6edfa6c60bae26de70b1
Hao Nguyen <ndkimhao@users.noreply.github.com>
2022-11-11 19:25:09 -0600

    analyze.sh: auto pull

--- analyze.sh
+++ analyze.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+(
+
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+
+git pull
+
 for d in /hao/apps/storj/h1storj*; do
 	echo "DIR = $d"
 	rsync -a $d/storage/storage/{*.db,*.db-*} copied_db/
 	./earnings.py copied_db/
 	rm -r copied_db/
 done
+
+)
+
