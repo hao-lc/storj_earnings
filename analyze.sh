058ee9b77248ae1ed4b2960f2118450488e975cc
Hao Nguyen <ndkimhao@users.noreply.github.com>
2022-11-11 19:25:30 -0600

    analyze.sh: passthrough arg

--- analyze.sh
+++ analyze.sh
@@ -9,7 +9,7 @@ git pull
 for d in /hao/apps/storj/h1storj*; do
 	echo "DIR = $d"
 	rsync -a $d/storage/storage/{*.db,*.db-*} copied_db/
-	./earnings.py copied_db/
+	./earnings.py copied_db/ "$@"
 	rm -r copied_db/
 done
 
