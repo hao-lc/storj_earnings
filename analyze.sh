bb0ab441b5b5d642b8f099ccfe545ce41fcaa8ed
ndkimhao <ndkimhao@users.noreply.github.com>
2022-11-28 02:41:31 -0600

    Update analyze.sh

--- analyze.sh
+++ analyze.sh
@@ -7,9 +7,9 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 git config pull.ff only
 git pull
 
-: ${PREFIX:=/hao/apps/storj/h1storj}
+: ${PREFIX:=/hao/apps/storj/h1storj*}
 
-for d in ${PREFIX}*; do
+for d in ${PREFIX}; do
 	echo "DIR = $d"
 	rsync -a $d/storage/storage/{*.db,*.db-*} copied_db/
 	./earnings.py copied_db/ "$@"
